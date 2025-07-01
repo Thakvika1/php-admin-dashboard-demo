9dba01688d1e1c020408613681fb8d7302248786
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-01 23:02:20 +0700

    add style to file

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -19,24 +19,42 @@
         file_put_contents($file_user_data, json_encode($users, JSON_PRETTY_PRINT));
     }
 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./src/dashboard.css">
+</head>
+<body>
+
+    <?php
     foreach($users as $name => $email) {
-        // echo "User No".($index + 1). "<br />";
+        echo '<div class="user-box">';
         echo "Name: ". htmlspecialchars($name)."<br />";
         echo "Email: ".htmlspecialchars($email['email'])."<br />";
-
         echo '
-        <form method="post" style="display:inline;">
-            <input type="hidden" name="delete_user" value='.$name.'>
-            <button type="submit">Delete</button>
-        </form>
-        <br /> <br /><br />
+            <form method="post" style="display:inline;">
+                <input type="hidden" name="delete_user" value="'.$name.'">
+                <button type="submit">Delete</button>
+            </form>
         ';
+        echo '</div>';
+
     }
 
 ?>
 
-
     <h1>
         <a href="web.php">Go back to application</a>
     </h1>
+    
+</body>
+</html>
+
+
+
+
     
